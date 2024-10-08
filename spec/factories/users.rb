c7392735bc089c4a36cb25ec8a7378321178a08c
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-08 20:24:53 +0900

    '1a'を追加&見やすくしました

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.name }
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { '1a' + Faker::Internet.unique.password(min_length: 6) }
     password_confirmation {password}
-    family_name { '山田' }
-    first_name { '太郎' }
-    family_name_kana { 'ヤマダ' }
-    first_name_kana { 'タロウ' }
-    birth { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    family_name           { '山田' }
+    first_name            { '太郎' }
+    family_name_kana      { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birth                 { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end

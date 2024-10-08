e11eb64d82840ac197db145b112ed6df18e09904
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-08 19:51:56 +0900

    編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname { Faker::Name.name }
-    email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
-    password { '123abc' }
-    password_confirmation { '123abc' }
+    nickname              { Faker::Name.name }
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
     family_name { '山田' }
     first_name { '太郎' }
     family_name_kana { 'ヤマダ' }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '#create' do
-    before do
-      @user = FactoryBot.build(:user)
-    end
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
-    context '新規登録できるとき' do
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
       it '全ての項目が存在すれば登録できる' do
         expect(@user).to be_valid
       end

a213852cddcd0334d834dbe851c5141f9ad23529
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-09 16:57:19 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+    private
+
+    def item_params
+      params.require(:item).permit(:name, :content,
+                                   :price, :category_id,
+                                   :condition_id, :shipping_fee_id,
+                                   :shipping_area_id, :shipping_date_id,
+                                   :image).merge(user_id: current_user.id)
+    end
 end

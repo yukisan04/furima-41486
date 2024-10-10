e1734b4c2cbbca9a6a7e3cf88f5337e3ba612496
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-10 20:50:34 +0900

    js実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+function count(){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    profitDom.innerHTML = Math.floor(inputValue - inputValue * 0.1);
+  });
+};
+
+window.addEventListener('load', count)

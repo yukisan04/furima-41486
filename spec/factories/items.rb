eb477b7c044629aab139edab14684a279e0de4d0
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-10 19:04:46 +0900

    見やすく編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Name.name }
-    content { Faker::Lorem.sentence }
-    price { Faker::Number.within(range: 300..9999999) }
-    category_id { Faker::Number.within(range: 2..12) }
-    condition_id { Faker::Number.within(range: 2..7) }
-    shipping_fee_id { Faker::Number.within(range: 2..3) }
-    shipping_date_id { Faker::Number.within(range: 2..4) }
-    prefecture_id { Faker::Number.within(range: 2..48) }
+    name              { Faker::Name.name }
+    content           { Faker::Lorem.sentence }
+    price             { Faker::Number.within(range: 300..9999999) }
+    category_id       { Faker::Number.within(range: 2..12) }
+    condition_id      { Faker::Number.within(range: 2..7) }
+    shipping_fee_id   { Faker::Number.within(range: 2..3) }
+    shipping_date_id  { Faker::Number.within(range: 2..4) }
+    prefecture_id     { Faker::Number.within(range: 2..48) }
     association :user
 
     after(:build) do |item|

969b62eb97ad0863f48805b0aecc8a6a201d1432
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-11 18:19:57 +0900

    商品一覧表示機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
+    @items = Item.includes(:user).order(id: 'DESC')
   end
 
   def new

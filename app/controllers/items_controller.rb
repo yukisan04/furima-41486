8722d3fb10a1059ddc63354e492e8f405ebb0278
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-14 18:09:08 +0900

    修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :item_find, only: [:show, :set_item, :edit, :update]
+  before_action :item_find, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -41,11 +41,6 @@ class ItemsController < ApplicationController
                                  :condition_id, :shipping_fee_id,
                                  :prefecture_id, :shipping_date_id,
                                  :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    item = Item.find(params[:id])
-    redirect_to action: :index unless current_user.id == item.user_id
   end
 
   def item_find

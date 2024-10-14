f9ba574a7635718cfb0092dc6b745275cc3c6c1c
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-14 15:48:21 +0900

    商品情報編集機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :destroy, :update]
+  before_action :set_item, except: [:index, :new]
+
   def index
     @items = Item.includes(:user).order(created_at: :desc)
   end
@@ -9,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.save
       redirect_to action: :index
     else
@@ -21,6 +23,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
@@ -29,6 +39,15 @@ class ItemsController < ApplicationController
                                  :condition_id, :shipping_fee_id,
                                  :prefecture_id, :shipping_date_id,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    redirect_to action: :index unless current_user.id == item.user_id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end

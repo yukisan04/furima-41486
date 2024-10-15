d48773d8de7c65e905602edd8de1288f6c980615
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-15 13:32:15 +0900

    editがうまく動かなかったので修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless current_user.id == item.user_id
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def create

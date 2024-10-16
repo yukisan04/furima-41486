9a3a5ce6820aa458cb7fe1bc88f11b1072a0baf3
yukisan04 <yuki28yukinko2525@icloud.com>
2024-10-16 14:58:35 +0900

    ordersコントローラー作成

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,10 +1,14 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :hoge,:fuga...
+  attr_accessor :city, :post_code, :prefecture_id, :house_number, :building_name, :phone_number
 
-  # ここにバリデーションの処理を書く
-
-  def save
-    # 各テーブルにデータを保存する処理を書く
+  with options presence: true do
+    validates :city
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id
+    validates :house_number
+    validates :phone_number
   end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end
